59b41573dee6bd3c9bb1c4af10bb3113fada75b8
JongHyun <bobi09361@gmail.com>
2025-09-18 16:28:44 +0900

    Learn expanded

---
+++ lib/expanded.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SizedBox(
+          height: double.infinity,
+          child: Column(
+            // 주축 정렬 지정
+            mainAxisAlignment: MainAxisAlignment.center,
+
+            // 반대축 정렬 지정
+            crossAxisAlignment: CrossAxisAlignment.center,
+
+            children: [
+              Expanded(child: Container(color: Colors.blue)),
+              Expanded(child: Container(color: Colors.red)),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
