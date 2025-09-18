2c36035840d6c7d6f807e6f6cfbe0a941719888b
JongHyun <bobi09361@gmail.com>
2025-09-18 16:26:12 +0900

    Learn column

--- lib/column.dart
+++ lib/column.dart
@@ -1,31 +1,3 @@
-// import 'package:flutter/material.dart';
-
-// void main() {
-//   runApp(MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         body: SizedBox(
-//           width: double.infinity,
-//           height: double.infinity,
-//           child: Column(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: [
-
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 
 void main() {
