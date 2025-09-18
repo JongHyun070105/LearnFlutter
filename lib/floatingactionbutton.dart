b2b1ae1cc83a2110380dc8b7ba8b3244f0fc4d55
JongHyun <bobi09361@gmail.com>
2025-09-18 15:55:05 +0900

    Learn floatingactionbutton

---
+++ lib/floatingactionbutton.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FloatingActionButton(
+                onPressed: () {},
+                child: Icon(Icons.fmd_good),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
