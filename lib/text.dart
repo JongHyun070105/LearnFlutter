8e19b8b4d2594ecc33fd5d637672fe750a56c31f
JongHyun <bobi09361@gmail.com>
2025-09-20 22:19:04 +0900

    Learn text

--- lib/text.dart
+++ lib/text.dart
@@ -11,19 +11,50 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text(
-            '안녕하세요',
-            style: TextStyle(
-              // 글자 크기
-              fontSize: 60,
+        body: SizedBox(
+          child: Column(
+            children: [
+              Text(
+                '안녕하세요',
+                style: TextStyle(
+                  // 글자 크기
+                  fontSize: 60,
 
-              // 글자 두께
-              fontWeight: FontWeight.w800,
+                  // 글자 두께
+                  fontWeight: FontWeight.w800,
 
-              // 글자 색
-              color: Colors.blue,
-            ),
+                  // 글자 색
+                  color: Colors.blue,
+                ),
+              ),
+
+              // 밑줄
+              Text(
+                "안녕하세요",
+                style: TextStyle(
+                  fontSize: 60,
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+
+              // 윗줄
+              Text(
+                "안녕하세요",
+                style: TextStyle(
+                  fontSize: 60,
+                  decoration: TextDecoration.overline,
+                ),
+              ),
+
+              // 취소선
+              Text(
+                "안녕하세요",
+                style: TextStyle(
+                  fontSize: 60,
+                  decoration: TextDecoration.lineThrough,
+                ),
+              ),
+            ],
           ),
         ),
       ),
