4c2f924d1cb2008288413a1ee256184461e7a73a
JongHyun <bobi09361@gmail.com>
2025-10-16 10:07:16 +0900

    Learn listview 40,41번째 줄 유형지정 오류 as type으로 해결

--- lib/widget/listview.dart
+++ lib/widget/listview.dart
@@ -1,22 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(TestView());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class TestView extends StatefulWidget {
+  const TestView({super.key});
+
+  @override
+  State<TestView> createState() => _TestViewState();
+}
+
+class _TestViewState extends State<TestView> {
+  final postList = [
+    {"title": "Title 1", "color": Colors.yellow},
+    {"title": "Title 2", "color": Colors.blue},
+    {"title": "Title 3", "color": Colors.black},
+  ];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: ListView(
-          scrollDirection: Axis.vertical,
-          children: [
-            postContainer(title: "title1", colorName: Colors.yellow),
-            postContainer(title: "title2", colorName: Colors.red),
-          ],
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              const DrawerHeader(
+                decoration: BoxDecoration(color: Colors.blue),
+                child: Text("Drawer Header Part"),
+              ),
+              ListTile(title: Text("Menu 1")),
+            ],
+          ),
+        ),
+        body: ListView.builder(
+          itemCount: postList.length,
+          itemBuilder: (BuildContext context, int index) {
+            return postContainer(
+              title: postList[index]["title"] as String,
+              colorName: postList[index]["color"] as dynamic,
+            );
+          },
         ),
       ),
     );
